6e6ed8cd5d5b156b0fc82ea04e1beb337cdbb6c3
Kamerynn Harrah <kamharrah@gmail.com>
2017-04-28 09:30:23 -0400

    Day 5 part 2

--- day5/solution.js
+++ day5/solution.js
@@ -1,12 +1,25 @@
 var crypto = require('crypto');
 
+// Safely counts sparse arrays
+function count(array) {
+    var total = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (i in array) {
+            total++;
+        }
+    }
+
+    return total;
+}
+
 // Initial input
 var doorID = 'ugkcyxxp';
 
 var passwordChars = [];
+var passwordDesiredLength = 8;
 var index = 0;
 
-while (passwordChars.length < 8) {
+while (count(passwordChars) < passwordDesiredLength) {
     // Append the index to door ID
     var toHash = doorID + index;
 
@@ -15,9 +28,23 @@ while (passwordChars.length < 8) {
 
     // If the hash has 5 leading zeroes
     if (hash.substr(0, 5) === '00000') {
-        // Store the 6th char
+        console.log('valid hash found');
         console.log(hash);
-        passwordChars.push(hash.substr(5, 1))
+
+        // The 6th character is the position
+        var positionString = hash.substr(5, 1);
+        var position = parseInt(positionString);
+
+        // If the position is valid, and nothing exists yet in that position
+        if (!isNaN(position) &&
+            position < passwordDesiredLength &&
+            passwordChars[position] === undefined) {
+
+            console.log('valid position found');
+            console.log(hash);
+
+            passwordChars[position] = hash.substr(6, 1);
+        }
     }
 
     index++;
